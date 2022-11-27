963d4e1719fd68376798e52ee8f42ea5c4daf51b
Nikodim-monk <lr1975@yandex.ru>
2022-11-27 19:44:20 +0400

    начало

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -3,15 +3,21 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 import com.javamentor.qa.platform.models.dto.AnswerDto;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.VoteAnswerService;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,16 +25,14 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/user/question/{questionId}/answer")
 public class ResourceAnswerController {
 
     private final AnswerDtoService answerDtoService;
     private final QuestionService questionService;
-
-    public ResourceAnswerController(AnswerDtoService answerDtoService, QuestionService questionService) {
-        this.answerDtoService = answerDtoService;
-        this.questionService = questionService;
-    }
+    private final VoteAnswerService voteAnswerService;
+    private final AnswerService answerService;
 
     @Operation(
             summary = "Получение списка ответов",
@@ -46,6 +50,20 @@ public class ResourceAnswerController {
             return new ResponseEntity<>(answerDtoService.getAllAnswersDtoByQuestionId(questionId, user.getId()), HttpStatus.OK);
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+    @ApiOperation("Увеличивает оценку ответа")
+    @Operation(summary = "Увеличивает оценку ответа")
+    @ApiResponse(responseCode = "200", description = "Оценка ответа увеличена, репутация автора повышена")
+    @ApiResponse(responseCode = "400", description = "Ответ не найден")
+    @PostMapping("/{id}/upVote")
+    public ResponseEntity<Long> upVote(@Parameter(description = "id ответа для поднятие оценки")
+                                       @PathVariable("id") Long answerId, @AuthenticationPrincipal User user) {
+        try {
+            return ResponseEntity.ok(voteAnswerService.voteUp(answerService
+                    .getAnswerForVote(answerId, user.getId()).get(), user));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 }
